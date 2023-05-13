9ebd100a8fa127f08001e76bc97e0adaae14fc84
крутойчел123ярусский <132725963+anonpon@users.noreply.github.com>
2023-05-13 11:05:11 +0800

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,3 +1,12 @@
+from pygame import *
+window = display.set_mode((700, 500))
+background = transform.scale(
+    image.load('background.png'), (700, 500))
+window.blit(background, (0, 0))
+
+
+
+ 
 class GameSprite(sprite.Sprite): #создание класса геймпспрайт наследника спрайт
     def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height): #конструктор инит
         super().__init__() #функции суперкласса
@@ -12,10 +21,31 @@ class GameSprite(sprite.Sprite): #создание класса геймпспр
         window.blit(self.image, (self.rect.x, self.rect.y))
     
 class Player(GameSprite): #создание класса плеер наследника геймпспрайт
-    def update(self): #метод апдейт для передвижения
+    def update_l(self): #метод апдейт для передвижения
         keys_pressed = key.get_pressed() #получаем все события клавиатуры
-        if keys_pressed[K_a] and self.rect.x > 0: #если нажата клавиша "a" и координаты больше нуля
-            self.rect.x -= self.speed  #то плеер передвигается влево
-        if keys_pressed[K_d] and self.rect.x < 630: #если нажата клавиша "d" и координаты игрока меньше 630 
-            self.rect.x += self.speed #то игрок передвигается в право
-   
+        if keys_pressed[K_w] and self.rect.y > 0: #если нажата клавиша "a" и координаты больше нуля
+            self.rect.y -= self.speed  #то плеер передвигается влево
+        if keys_pressed[K_s] and self.rect.y < 630: #если нажата клавиша "d" и координаты игрока меньше 630 
+            self.rect.y += self.speed #то игрок передвигается в право
+    def update_r(self): #метод апдейт для передвижения
+        keys_pressed = key.get_pressed() #получаем все события клавиатуры
+        if keys_pressed[K_UP] and self.rect.y > 0: #если нажата клавиша "a" и координаты больше нуля
+            self.rect.y -= self.speed  #то плеер передвигается влево
+        if keys_pressed[K_DOWN] and self.rect.y < 630: #если нажата клавиша "d" и координаты игрока меньше 630 
+            self.rect.y += self.speed #то игрок передвигается в право
+Lraketka = Player('raketka.png', 50, 100, 5, 50, 300)
+Rraketka = Player('raketka.png', 600, 100, 5, 50, 300)
+clock = time.Clock()
+FPS = 60
+game = True
+while game:
+    window.blit(background, (0, 0))
+    Lraketka.reset()
+    Rraketka.reset()
+    Lraketka.update_l()
+    Rraketka.update_r()
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
